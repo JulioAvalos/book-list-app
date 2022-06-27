eccb1abe4a8c43ba3ef03f5a9ddf3328fada79c2
JulioAvalos <julioavalos2@msn.com>
2022-06-27 00:17:25 -0600

    haciendo pruebas a las rutas de navegacion

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent  {
+export class NavComponent {
+
+  constructor(private router: Router) {
+  }
+
+  navTo(path: string): void {
+    this.router.navigate([`/${path}`]);
+  }
 
 }
